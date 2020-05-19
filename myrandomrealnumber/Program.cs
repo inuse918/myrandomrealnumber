d201fc3bc29bfc3552c13fb6fd3f5a93c6c40d4f
chaerin918 <s2018s34@e-mirim.hs.kr>
2020-05-19 11:11:29 +0900

    숫자 맞추기 프로그램

--- myrandomrealnumber/Program.cs
+++ myrandomrealnumber/Program.cs
@@ -10,6 +10,27 @@ namespace myrandomrealnumber
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            var input = 0;
+            //Console.WriteLine(random.Next(1,1000));
+            var com = random.Next(1, 1000);
+            Console.WriteLine(com);
+            while (true)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                input = int.Parse(Console.ReadLine());
+                if (input == com)
+                {
+                    Console.WriteLine("정답입니다!");
+                    break;
+                } else if (input < com)
+                {
+                    Console.WriteLine(input + "보다는 큰 숫자입니다.");
+                } else
+                {
+                    Console.WriteLine(input + "보다는 작은 숫자입니다.");
+                }
+            }
         }
     }
 }
